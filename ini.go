a50c85d34daf723bd3b19e09f749a5a1da0bc7cd
juan131 <juan@bitnami.com>
2020-05-07 10:36:07 +0200

    Run chown operations only when needed

--- ini.go
+++ ini.go
@@ -63,7 +63,7 @@ func iniSave(filename string, iniFile *ini.File) error {
 	if err := os.Chmod(f.File.Name(), mode); err != nil {
 		return err
 	}
-	if os.Geteuid() == 0 {
+	if os.Geteuid() != uid || os.Getegid() != gid {
 		if err := os.Chown(f.File.Name(), uid, gid); err != nil {
 			return err
 		}
